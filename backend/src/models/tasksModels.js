8ca3f40b65a2b6d1079091020ab58f5c225d9d29
Samuel Brito <sambriton7@gmail.com>
2023-05-15 18:43:49 -0300

    Some increments in the code

--- backend/src/models/tasksModels.js
+++ backend/src/models/tasksModels.js
@@ -1,8 +1,8 @@
 const conection = require('./conection')
 
 const getAll = async () => {
-    const tasks = await conection.execute('SELECT * FROM tasks')
-    return tasks[0]
+    const [tasks] = await conection.execute('SELECT * FROM tasks')
+    return tasks
 
 }
 
