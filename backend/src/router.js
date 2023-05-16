de369cf8305a3348dbc5c7bb2d61737d2a37a03d
Samuel Brito <sambriton7@gmail.com>
2023-05-16 17:09:06 -0300

    Creation of a middleware

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const taskControllers = require('./controllers/taskControllers')
+const tasksMiddlewares = require('./middlewares/tasksMiddlewares')
 
 const router = express.Router()
 
 router.get('/tasks', taskControllers.getAll)
 
-router.post('/tasks', taskControllers.createTask)
+router.post('/tasks', tasksMiddlewares.validateBody, taskControllers.createTask)
 
 module.exports = router
